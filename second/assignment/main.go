ee47b81d90aa2870b941f6b9451297631ed5eab6
rezapyon <juliansyahmreza@gmail.com>
2019-08-21 15:36:15 +0700

    Multiple Assignment

--- second/assignment/main.go
+++ second/assignment/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	//=========================SIngle Assignment=====================================//
 	var (
 		name   string
 		age    int
@@ -22,4 +26,26 @@ func main() {
 
 	fmt.Println("Previous Name: ", prevName)
 	fmt.Println("Current Name: ", name)
+
+	//=========================Multiple Assignment=====================================//
+
+	var (
+		speed int
+		now   time.Time
+	)
+
+	speed, now = 100, time.Now()
+
+	fmt.Println(speed, now)
+
+	//swapping variables
+
+	var (
+		speeds    = 100
+		prevSpeed = 50
+	)
+
+	speeds, prevSpeed = prevSpeed, speeds
+
+	fmt.Println(speeds, prevSpeed)
 }
