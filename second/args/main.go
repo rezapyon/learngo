0f59bb63786ca42e8d2b01b2e421690e890beefd
rezapyon <juliansyahmreza@gmail.com>
2019-08-22 11:28:50 +0700

    Arguments exercise

--- second/args/main.go
+++ second/args/main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func main() {
-	fmt.Printf("%#v\n", os.Args)
-	fmt.Println("Path:", os.Args[0])
-	fmt.Println("1st Argument:", os.Args[1])
-	fmt.Println("2nd Argument:", os.Args[2])
-	fmt.Println("3rd Argument:", os.Args[3])
+	// fmt.Printf("%#v\n", os.Args)
+	// fmt.Println("Path:", os.Args[0])
+	// fmt.Println("1st Argument:", os.Args[1])
+	// fmt.Println("2nd Argument:", os.Args[2])
+	// fmt.Println("3rd Argument:", os.Args[3])
 
-	fmt.Println("Number of items inside os.Args:", len(os.Args))
+	// fmt.Println("Number of items inside os.Args:", len(os.Args))
 	// $ go run main.go chaeyoung is beautiful
+
+	name := os.Args[1]
+	fmt.Println("Hello great", name, "!")
+
+	name, age := "Gandalf", 2019
+	fmt.Println("My name is", name)
+	fmt.Println("My age is", age)
+	fmt.Println("BTW, you shall pass!")
+
 }
