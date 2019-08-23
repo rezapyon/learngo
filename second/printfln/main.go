efddc243b90b8e21a3d3cb8fb8d9c97aafc7e4dc
rezapyon <juliansyahmreza@gmail.com>
2019-08-23 14:54:11 +0700

    printf safe-type tools

--- second/printfln/main.go
+++ second/printfln/main.go
@@ -22,9 +22,22 @@ func main() {
 		hasLife  = false
 	)
 
-	fmt.Printf("Planet: %v\n", planet)
-	fmt.Printf("Distance: %v millions kms\n", distance)
-	fmt.Printf("Orbital Period: %v days\n", orbital)
-	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
-	fmt.Printf("%v is %v away. Think! %[2]v kms! %[1]v OMG!\n", planet, distance)
+	// fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Planet: %s\n", planet)
+	// fmt.Printf("Distance: %v millions kms\n", distance)
+	fmt.Printf("Distance: %d millions kms\n", distance)
+	// fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Orbital Period: %f days\n", orbital)
+	// fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+	fmt.Printf("Does %s have life? %t\n", planet, hasLife)
+	// fmt.Printf("%v is %v away. Think! %[2]v kms! %[1]v OMG!\n", planet, distance)
+	fmt.Printf("%s is %d away. Think! %[2]v kms! %[1]v OMG!\n", planet, distance)
+
+	//formating float number
+	fmt.Printf("Orbital Period: %.0f days\n", orbital)
+	fmt.Printf("Orbital Period: %.1f days\n", orbital)
+	fmt.Printf("Orbital Period: %.2f days\n", orbital)
+	fmt.Printf("Orbital Period: %.3f days\n", orbital)
+	fmt.Printf("Orbital Period: %.4f days\n", orbital)
+	fmt.Printf("Orbital Period: %.5f days\n", orbital)
 }
