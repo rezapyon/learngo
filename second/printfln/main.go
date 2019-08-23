2e914930bb49e09554a23e684a607f363191a18a
rezapyon <juliansyahmreza@gmail.com>
2019-08-23 11:07:50 +0700

    printf and escape sequence

---
+++ second/printfln/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		speed int
+		heat  float64
+		off   bool
+		brand string
+	)
+
+	fmt.Printf("%T\n", speed)
+	fmt.Printf("%T\n", heat)
+	fmt.Printf("%T\n", off)
+	fmt.Printf("%T\n", brand)
+
+	var (
+		planet   = "Venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Distance: %v millions kms\n", distance)
+	fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+}
