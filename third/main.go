e2a659b21da7bc9ac8979eeb6a3fa992d0a1d762
rezapyon <juliansyahmreza@gmail.com>
2019-08-27 14:17:23 +0700

    celcius to fahrenheit

--- third/main.go
+++ third/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("dif :", 3-0.5)  //difference float64
 	fmt.Println("prod :", 4*5)   //product int
 	fmt.Println("prod :", 5*2.5) //product float64
-	fmt.Println("quot :", 8/2)   //quotient int
+	fmt.Println("quot :", 8/2)   //quotient intgit
 	fmt.Println("quot :", 8/1.5) //quotient float64
 
 	fmt.Println("rem :", 8%3) //remainder only for ints
@@ -23,5 +23,10 @@ func main() {
 
 	ratio = float64(3) / 2
 	fmt.Println(ratio) //result is 1.5
+
+	celcius := 35.
+	fahrenheit := (9*celcius + 160) / 5
+
+	fmt.Printf("%g C is %g F\n", celcius, fahrenheit)
 
 }
