c5f66b4cdfa23db07ad306c48c8d5e260427725d
rezapyon <juliansyahmreza@gmail.com>
2019-09-04 15:54:00 +0700

    Learn underlying types

--- fourth/main.go
+++ fourth/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rezapyon/learngo/fourth/weights"
+)
 
 type gram float64
 type ounce float64
@@ -12,4 +16,34 @@ func main() {
 	o = ounce(g) * 0.035274
 
 	fmt.Printf("%g grams is %.2f ounces\n", g, o)
+
+	type (
+		Gram     int
+		Kilogram Gram
+		Ton      Kilogram
+	)
+
+	var (
+		salt   Gram     = 100
+		apples Kilogram = 5
+		truck  Ton      = 10
+	)
+
+	fmt.Printf("salt: %d, apples: %d, truck: %d\n", salt, apples, truck)
+
+	salt = Gram(apples)
+	apples = Kilogram(truck)
+	truck = Ton(Gram(int(apples)))
+
+	salt = Gram(weights.Gram(100))
+
+	fmt.Printf("Type of weight.Gram: %T\n", weights.Gram(1))
+	fmt.Printf("Type of weight.Gram: %T\n", Gram(1))
+
+	type myGram weights.Gram
+
+	var pepper myGram = 20
+	pepper = myGram(salt)
+
+	fmt.Printf("Type of pepper : %T\n", pepper)
 }
---
+++ fourth/weights/weights.go
@@ -0,0 +1,12 @@
+package weights
+
+type (
+	// Gram underlying type is int
+	Gram int
+
+	// Kilogram underlying type is int
+	Kilogram Gram
+
+	// Ton underlying type is int
+	Ton Kilogram
+)
